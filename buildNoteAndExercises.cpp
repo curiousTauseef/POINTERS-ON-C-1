cde05447cc78f1e269d8bb02bf402c99c5c0dd58
Anilople <Anilople@outlook.com>
2016-09-22 23:59:41 +0800

    建立笔记和练习题

---
+++ buildNoteAndExercises.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+char *toCharArray(const string &it)
+{
+	int length = it.length();
+	char *ans = new char[length + 1];
+	ans[length] = '\0';
+	for (int i = 0; i < length; i++)
+		ans[i] = it[i];
+	return ans;
+}
+string numToString(int it)
+{
+	string ans = "";
+	while (it > 0)
+	{
+		int temp = it % 10;
+		ans += (char)(temp + 48);
+		it /= 10;
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+int main()
+{
+	int chapter, numOfPractice;
+	cout << "输入章节:";
+	cin >> chapter;
+	cout << "输入题目数量:";
+	cin >> numOfPractice;
+	string prefix = "copy nul " + numToString(chapter) + ".";
+	string note = prefix + "note.txt";
+	system(toCharArray(note));
+	for (int i = 1; i <= numOfPractice; i++)
+	{
+		string practice = numToString(i) + ".c";
+		system(toCharArray(prefix + practice));
+	}
+	//string a = "pause";
+	//system(toCharArray(a));
+	return 0;
+}
